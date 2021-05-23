6c8c2b6488eacfbdf06cc0bdeb396dcdd3bf32aa
NurFirdausR <nurfirdaus.5000@gmail.com>
2021-05-23 11:27:52 +0700

    finish

--- UKK-PM/resources/views/petugas/masyarakat/laporan.blade.php
+++ UKK-PM/resources/views/petugas/masyarakat/laporan.blade.php
@@ -1,69 +1,69 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="page-header page-header-light">
+<div class="page-header page-header-light">
 
 
-        <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
-            <div class="d-flex">
-                <div class="breadcrumb">
-                    <a href="{{ route('petugas.dashboard') }}" class="breadcrumb-item"><i class="icon-home2 mr-2"></i>
-                        Home</a>
-                    <span class="breadcrumb-item active">Laporan</span>
-                    <span class="breadcrumb-item active">Laporan Masyarakat</span>
-                </div>
-
-                <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
+    <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+        <div class="d-flex">
+            <div class="breadcrumb">
+                <a href="{{ route('petugas.dashboard') }}" class="breadcrumb-item"><i class="icon-home2 mr-2"></i>
+                    Home</a>
+                <span class="breadcrumb-item active">Laporan</span>
+                <span class="breadcrumb-item active">Laporan Masyarakat</span>
             </div>
 
-            <div class="header-elements d-none">
-                <div class="breadcrumb justify-content-center">
-                 
-                    
-                </div>
+            <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
+        </div>
+
+        <div class="header-elements d-none">
+            <div class="breadcrumb justify-content-center">
+             
+                
             </div>
         </div>
     </div>
-    <br>
-        <div class="row justify-content-center">
-               
+</div>
+<br>
+    <div class="row justify-content-center">
+           
 
 
 
 
-                <div class="table-responsive col-10">
-                    <a href="{{route('pdf.masyarakat')}}" class="btn btn-danger">Export PDF</a>
+            <div class="table-responsive col-10">
+                <a href="{{route('pdf.masyarakat')}}" class="btn btn-danger">Export PDF</a>
 
-                    <div class="card-header header-elements-inline">
-                        <h4 class="card-title">Data Masyarakat</h4>
+                <div class="card-header header-elements-inline">
+                    <h4 class="card-title">Data Masyarakat</h4>
 
-                    </div>
-
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr class="text-center">
-                                <th>NO</th>
-                                <th>Nik</th>
-                                <th>Nama</th>
-                                <th>Telp</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($masyarakat as $mass)
-                                <tr>
-                                <td><b>{{ $loop->iteration }}</b></td>
-                                <td><b>{{ $mass->nik }}</b></td>
-                                <td><b>{{ $mass->nama }}</b></td>
-                                <td><b>{{ $mass->telp }}</b></td>
-                               
-                                </tr>
-
-
-                            @endforeach
-
-                        </tbody>
-                    </table>
-                    {{-- {{ $masyarakat->links() }} --}}
                 </div>
-    </div>
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr class="text-center">
+                            <th>NO</th>
+                            <th>Nik</th>
+                            <th>Nama</th>
+                            <th>Telp</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($masyarakat as $mass)
+                            <tr>
+                            <td><b>{{ $loop->iteration }}</b></td>
+                            <td><b>{{ $mass->nik }}</b></td>
+                            <td><b>{{ $mass->nama }}</b></td>
+                            <td><b>{{ $mass->telp }}</b></td>
+                           
+                            </tr>
+
+
+                        @endforeach
+
+                    </tbody>
+                </table>
+                {{-- {{ $masyarakat->links() }} --}}
+            </div>
+</div>
 @endsection
--- UKK-PM/resources/views/petugas/pengaduan/laporan.blade.php
+++ UKK-PM/resources/views/petugas/pengaduan/laporan.blade.php
@@ -25,79 +25,46 @@
         </div>
     </div>
     <br>
-    <div class="container">
-        <div class="row justify-content-center">
-            @extends('layouts.admin')
-
-        @section('content')
-            <div class="page-header page-header-light">
+    <div class="row justify-content-center">
 
 
-                <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
-                    <div class="d-flex">
-                        <div class="breadcrumb">
-                            <a href="{{ route('petugas.dashboard') }}" class="breadcrumb-item"><i
-                                    class="icon-home2 mr-2"></i>
-                                Home</a>
-                            <span class="breadcrumb-item active">Laporan</span>
-                            <span class="breadcrumb-item active">Laporan Pengaduan</span>
-                        </div>
 
-                        <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
-                    </div>
+        <div class="table-responsive col-10">
+            <a href="{{ route('pdf.pengaduan') }}" class="btn btn-danger">Export PDF</a>
+            <div class="card-header header-elements-inline">
+                <h4 class="card-title">Data Pengaduan</h4>
 
-                    <div class="header-elements d-none">
-                        <div class="breadcrumb justify-content-center">
-
-
-                        </div>
-                    </div>
-                </div>
             </div>
-            <br>
-            <div class="row justify-content-center">
-                
-                
-                
-                <div class="table-responsive col-10">
-                    <a href="{{route('pdf.pengaduan')}}" class="btn btn-danger">Export PDF</a>
-                    <div class="card-header header-elements-inline">
-                        <h4 class="card-title">Data Pengaduan</h4>
 
-                    </div>
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="text-center">
+                        <th>No</th>
+                        <th>Dari</th>
+                        <th>Judul pengaduan</th>
+                        <th>Tanggal dibuat</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($pengaduan as $pengadu)
+                        <tr class="text-dark text-center">
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $pengadu->Masyarakat->nama }}</td>
+                            <td>{{ $pengadu->judul_laporan }}</td>
+                            <td>{{ $pengadu->tgl_pengaduan }}</td>
+                            @if ($pengadu->status === 'selesai')
+                                <td><span class="badge badge-success">di tanggapi</span></td>
+                            @elseif($pengadu->status === '0')
+                                <td><span class="badge badge-danger">di tolak</span></td>
+                            @endif
+                        </tr>
+                    @endforeach
 
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr class="text-center">
-                                <th>No</th>
-                                <th>Dari</th>
-                                <th>Judul pengaduan</th>
-                                <th>Tanggal dibuat</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($pengaduan as $pengadu)
-                                <tr class="text-dark text-center">
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>{{$pengadu->Masyarakat->nama}}</td>
-                                    <td>{{$pengadu->judul_laporan}}</td>
-                                    <td>{{$pengadu->tgl_pengaduan}}</td>
-                                    @if ($pengadu->status === 'selesai')
-                                    <td><span class="badge badge-success">di tanggapi</span></td>
-                                    @elseif($pengadu->status === '0')
-                                    <td><span class="badge badge-danger">di tolak</span></td>
-                                    @endif
-                                </tr>
-                            @endforeach
-
-                        </tbody>
-                    </table>
-                    {{-- {{ $masyarakat->links() }} --}}
-                </div>
-            </div>
-        @endsection
-
+                </tbody>
+            </table>
+            {{-- {{ $masyarakat->links() }} --}}
+        </div>
     </div>
-</div>
+
 @endsection
